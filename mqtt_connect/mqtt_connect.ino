25005559d03e4404aa9de334fbb18d37aab4b504
SilkRoot <alienus@gmx.de>
2020-11-28 15:53:02 +0100

    add mqtt example

---
+++ mqtt_connect/mqtt_connect.ino
@@ -0,0 +1,62 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+ 
+const char* SSID = "Its even more fluffy";
+const char* PSK = "";
+const char* MQTT_BROKER = "192.168.0.122";
+ 
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+ 
+void setup() {
+    Serial.begin(9600);
+    setup_wifi();
+    client.setServer(MQTT_BROKER, 1883);
+}
+ 
+void setup_wifi() {
+    delay(10);
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(SSID);
+ 
+    WiFi.begin(SSID, PSK);
+ 
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+ 
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+ 
+void reconnect() {
+    while (!client.connected()) {
+        Serial.print("Reconnecting...");
+        if (!client.connect("ESP8266Client")) {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" retrying in 5 seconds");
+            delay(5000);
+        }
+    }
+}
+void loop() {
+ 
+    if (!client.connected()) {
+        reconnect();
+    }
+    client.loop();
+ 
+    snprintf (msg, 50, "Alive since %ld milliseconds", millis());
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("/home/data", "Hello World");
+    delay(5000);
+}
